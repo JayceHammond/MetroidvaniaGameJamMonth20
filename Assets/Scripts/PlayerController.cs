9ab3939864f28467aff87f79e5199a4a26cb748b
JayceHammond <hjayce6@gmail.com>
2023-05-16 20:25:55 -0500

    Jump changes + implement placeholder UI elements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,26 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jumpSpeed;
     public bool groundState;
+    public Rigidbody2D rb;
+    public float fCutJumpHeight;
     private void Start() {
 
     }
 
     private void FixedUpdate() {
-        
+        Move();
     }
 
     private void Update() {
-        RaycastHit hit;
 
+        if(Input.GetKeyDown(KeyCode.UpArrow) && groundState == true){
+            //transform.Translate(0, Mathf.Pow(10, jumpSpeed) * Time.deltaTime, 0);
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        }
+        
+    }
 
+    public void Move(){
         if(Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.down), 1.25f, LayerMask.GetMask("Ground"))){
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.down), Color.green);
             groundState = true;
@@ -34,10 +42,5 @@ public class PlayerController : MonoBehaviour
         else if(Input.GetKey(KeyCode.LeftArrow)){
             transform.Translate(-speed * Time.deltaTime, 0, 0);
         }
-
-        if(Input.GetKeyDown(KeyCode.UpArrow) && groundState == true){
-            transform.Translate(0, Mathf.Pow(10, jumpSpeed) * Time.deltaTime, 0);
-        }
-        
     }
 }
